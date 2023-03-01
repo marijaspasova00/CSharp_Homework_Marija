2dd4e4e25a316c9f74ef66fb370e33ced28a9b7d
marijaspasova00 <119892747+marijaspasova00@users.noreply.github.com>
2023-03-01 01:12:03 +0100

    Add project files.

---
+++ Homework_Class01_Exercise4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+Console.WriteLine("          *");
+Console.WriteLine("\n");
+Console.WriteLine("          *");
+Console.WriteLine("\n");
+Console.WriteLine("       *  *  *");
+Console.WriteLine("\n");
+Console.WriteLine("     *    *    *");
+Console.WriteLine("\n");
+Console.WriteLine("          *");
+Console.WriteLine("\n");
+Console.WriteLine("          *");
+Console.WriteLine("\n");
+Console.WriteLine("      *      *");
+Console.WriteLine("\n");
+Console.WriteLine("   *            *");
